c4fb7015168ac9f9c52d01f8d16aaef992190f71
Robby Alamsyah <robbyalamsyah025@gmail.com>
2022-05-06 14:36:52 +0700

    add conversion CameraImage to Image and face crop

--- lib/core/controllers/face_detection_controller.dart
+++ lib/core/controllers/face_detection_controller.dart
@@ -1,16 +1,20 @@
-import 'dart:ui';
+import 'dart:developer';
+import 'dart:typed_data';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/painting.dart';
 import 'package:get/get.dart';
 import 'package:google_mlkit_face_detection/google_mlkit_face_detection.dart';
+import 'package:image/image.dart';
+import 'package:mask_detection/core/utils/image_converter.dart';
 
 class FaceDetectionController extends GetxController {
   late final FaceDetectorOptions _detectorOptions;
   late final FaceDetector _detector;
 
   List<Rect> rectFaces = [];
+  List<Uint8List> facesData = [];
 
   bool _onDetection = false;
 
@@ -66,17 +70,24 @@ class FaceDetectionController extends GetxController {
       inputImageData: _inputImageData,
     );
 
-    _performDetection(_inputImage);
+    _performDetection(
+      inputImage: _inputImage,
+      cameraImage: cameraImage,
+    );
   }
 
-  Future<void> _performDetection(InputImage inputImage) async {
-    if (_onDetection) return;
+  Future<void> _performDetection({
+    required InputImage inputImage,
+    required CameraImage cameraImage,
+  }) async {
+    if (_onDetection || inputImage.bytes == null) return;
 
     _onDetection = true;
 
     final _faces = await _detector.processImage(inputImage);
 
     rectFaces.clear();
+    facesData.clear();
 
     for (Face face in _faces) {
       final _boundingBox = face.boundingBox;
@@ -87,10 +98,35 @@ class FaceDetectionController extends GetxController {
 
       final _rect = Rect.fromLTRB(_bLeft, _bTop, _bRight, _bBottom);
       rectFaces.add(_rect);
+
+      facesData.add(
+        _cropFaceRect(
+          cameraImage: cameraImage,
+          faceRect: _rect,
+        ),
+      );
     }
+
+    log(facesData.toString());
 
     _onDetection = false;
 
     update();
+  }
+
+  Uint8List _cropFaceRect({
+    required CameraImage cameraImage,
+    required Rect faceRect,
+  }) {
+    final _image = copyRotate(ImageConverter.convertToImage(cameraImage), -90);
+    final _faceCropped = copyCrop(
+      _image,
+      faceRect.left.round(),
+      faceRect.top.round(),
+      faceRect.width.round(),
+      faceRect.height.round(),
+    );
+
+    return Uint8List.fromList(encodePng(_faceCropped));
   }
 }
---
+++ lib/core/utils/image_converter.dart
@@ -0,0 +1,59 @@
+import 'dart:developer';
+
+import 'package:camera/camera.dart';
+import 'package:image/image.dart';
+
+abstract class ImageConverter {
+  static Image convertToImage(CameraImage image) {
+    try {
+      if (image.format.group == ImageFormatGroup.yuv420) {
+        return _convertYUV420(image);
+      } else if (image.format.group == ImageFormatGroup.bgra8888) {
+        return _convertBGRA8888(image);
+      }
+    } catch (e) {
+      log('error: $e');
+    }
+
+    throw Exception('Image format not supported');
+  }
+
+  static Image _convertBGRA8888(CameraImage image) {
+    return Image.fromBytes(
+      image.width,
+      image.height,
+      image.planes[0].bytes,
+      format: Format.bgra,
+    );
+  }
+
+  static Image _convertYUV420(CameraImage image) {
+    const hexFF = 0xFF000000;
+
+    final _width = image.width;
+    final _height = image.height;
+    final _img = Image(_width, _height);
+    final _uvRowStride = image.planes[1].bytesPerRow;
+    final _uvPixelStride = image.planes[1].bytesPerPixel;
+
+    for (int _x = 0; _x < _width; _x++) {
+      for (int _y = 0; _y < _height; _y++) {
+        final _uvIndex = _uvPixelStride! * (_x / 2).floor() +
+            _uvRowStride * (_y / 2).floor();
+        final _index = _y * _width + _x;
+        final _yp = image.planes[0].bytes[_index];
+        final _up = image.planes[1].bytes[_uvIndex];
+        final _vp = image.planes[2].bytes[_uvIndex];
+        final _r = (_yp + _vp * 1436 / 1024 - 179).round().clamp(0, 255);
+        final _g = (_yp - _up * 46549 / 131072 + 44 - _vp * 93604 / 131072 + 91)
+            .round()
+            .clamp(0, 255);
+        final _b = (_yp + _up * 1814 / 1024 - 227).round().clamp(0, 255);
+
+        _img.data[_index] = hexFF | (_b << 16) | (_g << 8) | _r;
+      }
+    }
+
+    return _img;
+  }
+}
